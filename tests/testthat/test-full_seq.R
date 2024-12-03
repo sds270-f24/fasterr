b0407bcec239e7e393450deb17df93a3de4d828a
Emma Ruckle <eruckle@smith.edu>
2024-12-03 11:48:04 -0500

    Full_seq tests.

---
+++ tests/testthat/test-full_seq.R
@@ -0,0 +1,23 @@
+test_that("rcpp full seq gives the same output as tidyr full seq on a small sequence", {
+  seq <- c(1, 3, 8)
+  step <- 1
+  expect_equal(tidyr::full_seq(seq, step), full_seq(seq, step))
+})
+
+test_that("rcpp full seq gives the same output as tidyr full seq on a large sequence", {
+  seq <- sample(1:1000, size = 500, replace = TRUE)
+  step <- 1
+  expect_equal(tidyr::full_seq(seq, step), full_seq(seq, step))
+})
+
+test_that("skipping benchmark tests", {
+  skip('skip')
+  seq <- sample(1:1000, size = 500, replace = TRUE)
+  step <- 1
+  bench::mark(
+    tidyr::full_seq(seq, step),
+    full_seq(seq, step)
+  )
+  system.time(replicate(1000, tidyr::full_seq(seq, step)))
+  system.time(replicate(1000, full_seq(seq, step)))
+})
