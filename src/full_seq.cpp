31b94bcf5629f91abc90a106915c1f7fc55a0dae
Emma Ruckle <eruckle@smith.edu>
2024-12-03 11:24:41 -0500

    Molly's function :).

---
+++ src/full_seq.cpp
@@ -0,0 +1,35 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+/**
+ *  Takes in an incomplete sequence and a step size and fills in the rest of the sequence based on the step size
+ *  Mimics functionality of tidyr::full_seq
+ * 
+ * @param x NumericVector normal sequence to be filled
+ * @param step double step between items in the sequence
+ * @return NumericVector filled sequence
+ */
+// [[Rcpp::export]]
+NumericVector full_seq(NumericVector x, double step) {
+  if (step <= 0) {
+    stop("Step size must be positive.");
+  }
+  
+  // sort input
+  std::sort(x.begin(), x.end());
+  
+  // get max and min
+  double min_val = x[0];
+  double max_val = x[x.size() - 1];
+  
+  // initialize vector with correct size
+  int n = static_cast<int>((max_val - min_val) / step) + 1;
+  NumericVector result(n);
+  
+  // fill vector
+  for (int i = 0; i < n; ++i) {
+    result[i] = min_val + i * step;
+  }
+  
+  return result;
+}
